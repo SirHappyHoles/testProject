2c750c11d6711492b3b83f5cc7ae28986decf26e
Sir Happy Holes <srhappyholes@gmail.com>
2026-01-10 08:39:11 -0300

    Atualizando

---
+++ Aula6/main.lua
@@ -0,0 +1,22 @@
+message = 0
+chicken = 0
+
+function increaseMessage(i)
+    message = message + i
+end
+
+function double(val)
+    val = val * 2
+    return val
+end
+
+message = double(12)
+chicken = double(3)
+
+--increaseMessage(30)
+--increaseMessage(5)
+
+function love.draw()
+    love.graphics.setFont(love.graphics.newFont(50))
+    love.graphics.print(message)
+end
---
+++ ExibirNome/main.lua
@@ -0,0 +1,6 @@
+nome = " "
+
+print("Digite o seu nome: ")
+nome = io.read()
+
+print("Ola seu nome é: " .. nome)
