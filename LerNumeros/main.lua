1d2d2f5a2c6d4a75cb3c7507655bd522cc34214e
Sir Happy Holes <srhappyholes@gmail.com>
2026-01-10 08:44:12 -0300

    Atualizando

---
+++ LerNumeros/main.lua
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+
+print("Digite um numero: ")
+a = tonumber(io.read())
+
+print("Digite outro numero: ")
+b = tonumber(io.read())
+
+print("O valor A é: " .. a)
+print("O valor B é: " .. b)
